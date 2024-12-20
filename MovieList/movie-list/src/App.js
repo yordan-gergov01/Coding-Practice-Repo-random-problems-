634779c1e1b17a1e49ece5577d2a9c84bf55839f
yordan-gergov01 <yordansoftskills@gmail.com>
2024-12-20 20:42:15 +0200

    create a list that shows when user enter new book

--- MovieList/movie-list/src/App.js
+++ MovieList/movie-list/src/App.js
@@ -1,19 +1,43 @@
 import { useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
   const [books, setBooks] = useState([]);
 
-  let newBook = {
-    id: Date.now(),
-    bookName: "",
-    isRead: false,
-  };
+  function handleAddNewBook() {
+    if (inputValue.trim() === "") return;
+
+    const newBook = {
+      id: Date.now(),
+      bookName: inputValue,
+      isRead: false,
+    };
+
+    setBooks([...books, newBook]);
+    setInputValue("");
+  }
 
   return (
     <div>
       <h1>My Book List</h1>
-      <input type="text" placeholder="Add new book" />
-      <button>Add Book</button>
+      <input
+        type="text"
+        placeholder="Add new book"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleAddNewBook}>Add Book</button>
+      <ul>
+        {books.map((book) => (
+          <>
+            <div>
+              <li>{book.bookName}</li>
+              <button>Delete</button>
+              <button>Finished</button>
+            </div>
+          </>
+        ))}
+      </ul>
     </div>
   );
 }
