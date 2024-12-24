a223817933849f12c13c686487b837511152e429
yordan-gergov01 <yordansoftskills@gmail.com>
2024-12-24 20:23:19 +0200

    create the summary view and transaction form

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,5 +1,14 @@
+import Summary from "./components/Summary";
+import TransactionForm from "./components/TransactionForm";
+
 function App() {
-  return <div>Hello React</div>;
+  return (
+    <div>
+      <h1>Expense Tracker 💸</h1>
+      <Summary />
+      <TransactionForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ expense-tracker/src/components/Summary.jsx
@@ -0,0 +1,12 @@
+function Summary() {
+  return (
+    <div>
+      <h2>Summary</h2>
+      <p>Income: 0$</p>
+      <p>Expenses: 0$</p>
+      <p>Balace: 0$</p>
+    </div>
+  );
+}
+
+export default Summary;
