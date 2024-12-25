ae077d8a3e78025b9c2f14556c9daf2a94a438d8
yordan-gergov01 <yordansoftskills@gmail.com>
2024-12-25 16:09:21 +0200

    calculate income and expenses

--- expense-tracker/src/components/Summary.jsx
+++ expense-tracker/src/components/Summary.jsx
@@ -1,10 +1,29 @@
-function Summary() {
+function Summary({ transactions }) {
+  const totalIncome = transactions
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((sum, transaction) => sum + transaction.amount, 0);
+
+  const totalExpenses = transactions
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((sum, transaction) => sum + transaction.amount, 0);
+
+  const balance = totalIncome - totalExpenses;
+
   return (
     <div>
       <h2>Summary</h2>
-      <p>Income: 0$</p>
-      <p>Expenses: 0$</p>
-      <p>Balace: 0$</p>
+      <p>
+        <strong>Income: </strong>
+        {totalIncome}$
+      </p>
+      <p>
+        <strong>Expenses: </strong>
+        {totalExpenses}$
+      </p>
+      <p>
+        <strong>Balance: </strong>
+        {balance}$
+      </p>
     </div>
   );
 }
