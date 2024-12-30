ed79b4e20cfef47991513599744d5e9d4704566f
yordan-gergov01 <yordansoftskills@gmail.com>
2024-12-30 14:51:06 +0200

    fetching the data from an API

--- recipe-finder/src/App.js
+++ recipe-finder/src/App.js
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+
 function App() {
-  return <div>Hello react</div>;
+  const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+  async function getRecipes(query) {
+    try {
+      const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setRecipes(data.meals);
+    } catch (err) {
+      console.error("Error fetching recipes:", err);
+    }
+  }
+
+  return (
+    <div className="app">
+      <h1>Recipe Finder 🍨</h1>
+      <SearchBar onSearch={getRecipes} />
+    </div>
+  );
 }
 
 export default App;
