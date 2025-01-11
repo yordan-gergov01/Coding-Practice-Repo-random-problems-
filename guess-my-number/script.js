e7b7fabff67f72704ae6a29e7a985b8d379fdd3f
yordan-gergov01 <yordansoftskills@gmail.com>
2025-01-11 10:36:20 +0200

    display the error message if the number is not exist in the input

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const button = document.querySelector('.check');
+button.addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      'Please start by writing a number...';
+  }
+});
