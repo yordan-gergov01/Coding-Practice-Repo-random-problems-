1eb4094f562759332e29e2e5f3ab1029e78b5093
yordan-gergov01 <yordansoftskills@gmail.com>
2025-01-12 08:41:15 +0200

    change the dice numbers with random number functionality

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,4 +1,16 @@
 'use strict';
 
-const playerOneScore = document.getElementById('#score--0');
-const playerTwoScore = document.getElementById('#score--1');
+const playerOneScore = document.getElementById('score--0');
+const playerTwoScore = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+const rollDiceButton = document.querySelector('.btn--roll');
+
+playerOneScore.textContent = 0;
+playerTwoScore.textContent = 0;
+dice.classList.add('hidden');
+
+rollDiceButton.addEventListener('click', () => {
+  const randomNumberForDice = Math.floor(Math.random() * 6) + 1;
+  dice.src = `dice-${randomNumberForDice}.png`;
+  dice.classList.remove('hidden');
+});
