b0d732cd590148ac6599d9ac30e865d8de46d21b
yordan-gergov01 <yordansoftskills@gmail.com>
2025-01-22 16:05:20 +0200

    implement lazy loading for images

--- BankistWebsite/script.js
+++ BankistWebsite/script.js
@@ -151,12 +151,12 @@ headerObserver.observe(header);
 const allSections = document.querySelectorAll('.section');
 
 function revealSection(entries, observer) {
-  const [entry] = entries;
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
 
-  if (!entry.isIntersecting) return;
-
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
 }
 
 const sectionObserver = new IntersectionObserver(revealSection, {
@@ -167,3 +167,29 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+function loadImg(entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
