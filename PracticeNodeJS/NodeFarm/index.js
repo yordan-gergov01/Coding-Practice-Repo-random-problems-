4375c248752ffa721b9e434b7bf299970a01e9ad
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-02 10:10:01 +0200

    initial commit with starter fiels

--- PracticeNodeJS/NodeFarm/index.js
+++ PracticeNodeJS/NodeFarm/index.js
@@ -5,6 +5,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const replaceTemplate = require("./modules/replaceTemplate");
+
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
 
@@ -28,22 +30,6 @@ const url = require("url");
 
 ////////////////////////////////////////////////////////////
 // SERVER
-function replateTemplate(temp, product) {
-  let output = temp.replace(/{%PRODUCTNAME%/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-}
-
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -70,7 +56,7 @@ const server = http.createServer((req, res) => {
     });
 
     const cardsHtml = productDataObj
-      .map((product) => replateTemplate(templateCard, product))
+      .map((product) => replaceTemplate(templateCard, product))
       .join("");
 
     const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
@@ -84,7 +70,7 @@ const server = http.createServer((req, res) => {
     });
 
     const product = productDataObj[query.id];
-    const output = replateTemplate(templateProduct, product);
+    const output = replaceTemplate(templateProduct, product);
 
     res.end(output);
 
