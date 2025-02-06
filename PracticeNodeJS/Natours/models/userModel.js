49d9aae3c5dfe232772b34df918d399a5763a4a1
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-06 11:36:06 +0200

    managing password with hashing

--- PracticeNodeJS/Natours/models/userModel.js
+++ PracticeNodeJS/Natours/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,7 +24,29 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This ONLY works on CREATE and SAVE!
+      // returns true if the both passwords are the same
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // only run this function if password was actually modified
+  // refers to the current user
+  if (!this.isModified('password')) return next();
+
+  // hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // to not to be persisted in the database
+  // delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
