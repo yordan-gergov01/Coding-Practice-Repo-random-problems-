71f4ab934d20ed9965558c25528011fc39d118bc
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-07 11:21:20 +0200

    setting new password

--- PracticeNodeJS/Natours/models/userModel.js
+++ PracticeNodeJS/Natours/models/userModel.js
@@ -60,6 +60,13 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 // compare the passwords in login
 userSchema.methods.correctPassword = async function (
   candidatePassword,
