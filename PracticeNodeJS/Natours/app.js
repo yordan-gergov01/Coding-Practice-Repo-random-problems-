205c94b078361aaf01eca7827a11c46457bd3a9d
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-07 19:43:29 +0200

    setting security http headers

--- PracticeNodeJS/Natours/app.js
+++ PracticeNodeJS/Natours/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,17 +10,26 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Global middlewares - function that can modify the request data
+// GLOBAL MIDDLEWARES - function that can modify the request data
+// Set security HTTP headers
+app.use(helmet());
+
+// development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+// limit  requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
+
 app.use('/api', limiter);
 
-app.use(express.json());
+// body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Standard way for routing
