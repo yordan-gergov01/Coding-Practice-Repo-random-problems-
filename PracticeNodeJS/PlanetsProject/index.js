7b5b5ddc6ea8fb74f94c367bf4d0206d3affdfe7
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-22 12:15:39 +0200

    finding habitable planets

--- PracticeNodeJS/PlanetsProject/index.js
+++ PracticeNodeJS/PlanetsProject/index.js
@@ -1,7 +1,17 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
 fs.createReadStream("kepler_data.csv", "utf8")
   .pipe(
     parse({
@@ -10,11 +20,13 @@ fs.createReadStream("kepler_data.csv", "utf8")
     })
   )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(results);
+    console.log(`${habitablePlanets.length} habitable planets found!`);
   });
