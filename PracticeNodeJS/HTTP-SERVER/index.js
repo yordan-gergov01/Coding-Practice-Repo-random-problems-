a9796a0e06f48b2de3ab5318b7ea41d28208b3da
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-23 09:01:45 +0200

    initial commit and creating a simple api with test routes

--- PracticeNodeJS/HTTP-SERVER/index.js
+++ PracticeNodeJS/HTTP-SERVER/index.js
@@ -5,7 +5,10 @@ const PORT = 3000;
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-  if (req.url === "/register") {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Your Progress Dashboard</h1>");
+  } else if (req.url === "/register") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(`
             <h1>Fitness Server App</h1>
@@ -16,9 +19,6 @@ server.on("request", (req, res) => {
             <button type='submit'>Register</button>
             </div>
             `);
-  } else if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Your Progress Dashboard</h1>");
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<h1>Page Not Found!</h1>");
