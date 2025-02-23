3973b14090a8da158f6ec8cd1226235e8da168ed
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-23 10:15:15 +0200

    create new friend with implementing middlewares

--- PracticeNodeJS/ExpressProject/server.js
+++ PracticeNodeJS/ExpressProject/server.js
@@ -15,6 +15,34 @@ const friends = [
   },
 ];
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: "The name does not exist",
+    });
+  }
+
+  const newFriend = {
+    id: friends.length,
+    name: req.body.name,
+  };
+
+  friends.push(newFriend);
+
+  res.status(200).json({
+    newFriend,
+  });
+});
+
 app.get("/friends", (req, res) => {
   res.json(friends);
 });
