97b59a62f1b96732551b3dea9db60375e4ba0404
yordan-gergov01 <yordansoftskills@gmail.com>
2025-03-09 11:00:30 +0200

    designing an e-commerce schema

--- PracticeNodeJS/GraphQLExample/server.js
+++ PracticeNodeJS/GraphQLExample/server.js
@@ -4,14 +4,63 @@ const { graphqlHTTP } = require("express-graphql");
 
 const schema = buildSchema(`
         type Query {
+          products: [Product]
+          orders: [Order]
+        }
+
+        type Product {
+            id: ID!
             description: String
-            price: Float
+            reviews: [Review]
+            price: Float!
+        }
+
+        type Review {
+            rating: Int!
+            comment: String
+        }
+
+        type Order {
+            date: String
+            subtotal: Float!
+            items: [OrderItem]
+        }
+
+        type OrderItem {
+            product: Product!
+            quantity: Int!
         }
     `);
 
 const root = {
-  description: "Red Shoe",
-  price: 42.12,
+  products: [
+    {
+      id: "redshoe",
+      description: "Red Shoe",
+      price: 42.12,
+    },
+    {
+      id: "bluejean",
+      description: "Blue Jeans",
+      price: 55.55,
+    },
+  ],
+  orders: [
+    {
+      date: "2005-05-05",
+      subtotal: 90.22,
+      items: [
+        {
+          product: {
+            id: "redshoe",
+            description: "Old Red Shoe",
+            price: 45.11,
+          },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
 };
 
 const app = express();
@@ -23,6 +72,7 @@ app.listen(PORT, () => {
     graphqlHTTP({
       schema: schema,
       rootValue: root,
+      graphiql: true,
     })
   );
   console.log(`Listening on port ${[PORT]}...`);
